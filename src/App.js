89a340f655ee7430b09d35de4d74cb808b17d4cb
Gabriel Anguiano <anguianog97@gmail.com>
2020-12-15 09:46:13 -0800

    universal styling complete for application.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import {ThemeProvider } from '@material-ui/core/styles'
-import {lightTheme, darkTheme} from './components/UniversalStyles'
-import Main from './components/Main'
-const  App = () => { 
-  const [darkmode, setDarkMode] = useState(false)
+import { ThemeProvider } from '@material-ui/core/styles';
+import { lightTheme, darkTheme } from './components/UniversalStyles';
+import Main from './components/Main';
 
-  
- 
+const App = () => {
+	const [ darkmode, setDarkMode ] = useState(false);
 
-
-  return (
-    
- 
-        <ThemeProvider theme ={darkmode ? darkTheme: lightTheme}> 
-           <CssBaseline/>
-            <Main/>
-            <button onClick={() => setDarkMode(prevState => !prevState)}>Toggle Theme</button>
-        </ThemeProvider> 
-      
-    
-
-  );
-}
+	return (
+		<ThemeProvider theme={darkmode ? darkTheme : lightTheme}>
+			<CssBaseline>
+				<Main />
+			</CssBaseline>
+		</ThemeProvider>
+	);
+};
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,10 @@
-import React from 'react'
+import React from 'react';
 const Main = () => {
-    return (
-        
-       <div>
-           <h1>Countries</h1>
-       </div>
+	return (
+		<div>
+			<h1>Countries</h1>
+		</div>
+	);
+};
 
-            
-    )
-}
-
-export default Main
+export default Main;
