ac9187106c2c17982df658218bde18879bd653e1
Gabriel Anguiano <anguianog97@gmail.com>
2020-12-16 08:45:53 -0800

    search input structure complete. need to work on styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { SearchInput } from './Search/Search';
 const Main = () => {
 	return (
 		<div>
 			<h1>Countries</h1>
+			<SearchInput />
 		</div>
 	);
 };
---
+++ src/components/Search/Search.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { useStyles } from './SearchStyles/SearchStyles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+export const SearchInput = () => {
+	const classes = useStyles();
+	return (
+		<form className={classes.root}>
+			<TextField
+				id='filled-basic'
+				placeholder='Search for a country...'
+				variant='filled'
+				fullWidth={true}
+				color='primary'
+			/>
+			<FontAwesomeIcon icon={faSearch} />
+		</form>
+	);
+};
