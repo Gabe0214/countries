688f82491880fcbd19ad6d03facc375c69d650e5
Gabriel Anguiano <anguianog97@gmail.com>
2020-12-22 09:12:54 -0800

    transfered countries-route styling to its own directory.

--- src/components/Countries/CountriesIndividual/CountriesIndividual.js
+++ src/components/Countries/CountriesIndividual/CountriesIndividual.js
@@ -2,79 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Box from '@material-ui/core/Box';
 import CardMedia from '@material-ui/core/CardMedia';
-import { makeStyles } from '@material-ui/core/styles';
+import { useStyles } from './CountriesIndividualStyles/CountriesIndividualStyles';
 import TrendingFlatIcon from '@material-ui/icons/TrendingFlat';
 import Typography from '@material-ui/core/Typography';
 import { NavLink } from 'react-router-dom';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import { CountryDetatils } from './CountryDetatils';
-
-const useStyles = makeStyles((theme) => ({
-	media: {
-		height: 220
-	},
-	back: {
-		display: 'flex',
-		alignItems: 'center',
-		backgroundColor: theme.palette.primary.main,
-		width: '35%',
-		padding: '7px 0px',
-		boxShadow: '0px 3px 3px -2px rgba(0,0,0,0.2),0px 3px 4px 0px rgba(0,0,0,0.14),0px 1px 8px 0px rgba(0,0,0,0.12)',
-		textDecoration: 'none',
-		justifyContent: 'center',
-		color: theme.palette.text.primary,
-		marginRight: '3%',
-
-		'& .MuiSvgIcon-root': {
-			transform: 'rotateY(180deg)',
-			marginRight: '8%',
-			fill: theme.palette.text.primary
-		},
-
-		'& .MuiTypography-button': {
-			fontWeight: '600'
-		}
-	},
-	listContainer: {
-		'& .MuiList-root': {
-			paddingTop: '0',
-			paddingBottom: '0'
-		},
-
-		'& .MuiListItem-gutters': {
-			paddingLeft: '0',
-			paddingRight: '0',
-			paddingBottom: '0',
-			paddingTop: '0'
-		},
-
-		'& .MuiTypography-subtitle1': {
-			fontWeight: '600',
-			marginRight: '2%'
-		},
-
-		'& .MuiListItemText-root': {
-			flex: 'none',
-			marginTop: '0',
-			marginBottom: '0',
-			fontSize: '0.8rem !important'
-		},
-
-		'& .MuiTypography-body1': {
-			fontSize: '0.9rem'
-		},
-
-		'& .MuiBox-root': {
-			marginTop: '8%'
-		}
-	},
-	btnBox: {
-		margin: '5% 0 6% 0 ',
-		display: 'flex'
-	}
-}));
 
 const CountriesIndividual = (props) => {
 	const [ countryDetail, setCountryDetail ] = useState({});
@@ -97,12 +30,6 @@ const CountriesIndividual = (props) => {
 
 		fetchCountry();
 	}, []);
-
-	// const keys = countryDetail.length > 0 ? Object.keys(countryDetail && countryDetail[0]) : null;
-	// // console.log(keys[16]);
-	// console.log(keys);
-	// console.log(countryDetail);
-	// console.log(countryDetail[keys[16]]);
 
 	if (Object.keys(countryDetail).length == 0) {
 		return <h2>Loading...</h2>;
@@ -142,8 +69,6 @@ const CountriesIndividual = (props) => {
 								currencies={countryDetail.currencies}
 								borderCountries={countryDetail.borders}
 							/>
-
-							{/* // ))} */}
 						</List>
 					</Box>
 				</Box>
