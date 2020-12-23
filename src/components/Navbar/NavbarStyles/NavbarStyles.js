b3a4e19d8ed816ae934d12bf9c740c2655bfdf64
Gabriel Anguiano <anguianog97@gmail.com>
2020-12-23 10:58:16 -0800

    navbar responsiveness for 1280 screen complete

--- src/components/Navbar/NavbarStyles/NavbarStyles.js
+++ src/components/Navbar/NavbarStyles/NavbarStyles.js
@@ -2,7 +2,18 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
 	root: {
-		flexGrow: 1
+		flexGrow: 1,
+
+		'& .MuiToolbar-root': {
+			[theme.breakpoints.up('lg')]: {
+				justifyContent: 'space-between',
+				width: '100%',
+				margin: '0 auto',
+				maxWidth: theme.breakpoints.values.lg,
+				paddingLeft: '0',
+				paddingRight: '0'
+			}
+		}
 	},
 	title: {
 		flexGrow: 1,
